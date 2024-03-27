a2386b2602c0b77d8d2c5f2e591acc118aead6a5
Akhilpp123 <161755318+Akhilpp123@users.noreply.github.com>
2024-03-27 18:40:00 +0530

    Update firebase.js

--- E-COMMERCE/src/firebase/firebase.js
+++ E-COMMERCE/src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_vGF8_HCKtWxe9dIGHMtvd8kbaCqfp4k",
-    authDomain: "e-learning-93e12.firebaseapp.com",
-    projectId: "e-learning-93e12",
-    storageBucket: "e-learning-93e12.appspot.com",
-    messagingSenderId: "907406566798",
-    appId: "1:907406566798:web:1bdd6c27466decd0dcb0df"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const app = initializeApp(firebaseConfig);
